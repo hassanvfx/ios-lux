4224b07ab55759ac9c3c135d45b7c5b185efab2e
eonfluxor <eonfluxor@gmail.com>
2021-03-06 21:08:59 -0800

    patch-pack

--- Package.swift
+++ Package.swift
@@ -6,19 +6,18 @@ import PackageDescription
 let package = Package(
     name: "Lux",
     platforms: [
+        .macOS(.v10_15),
         .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Lux",
-            type: .dynamic,
             targets: ["Lux"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Lux",
             path: "Lux"
