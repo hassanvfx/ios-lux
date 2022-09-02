d1bbff35a65aee2acf6be3bc2a395bb49fe026bf
hassan <hassanvfx@gmail.com>
2022-09-01 21:02:24 -0500

    chore

--- Package.swift
+++ Package.swift
@@ -9,18 +9,15 @@ let package = Package(
         .iOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Lux",
             targets: ["Lux"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Lux",
-            path: "Lux"
-        ),
+            dependencies: []
+        )
     ]
 )
